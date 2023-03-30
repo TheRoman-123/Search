61998dc2fd0fef603de79be83da6379461a1bd09
Roman Rudi <roman.rudi@outlook.com>
2023-03-30 10:17:06 +0300

    Write Javadoc for classes

--- src/main/java/org/example/SerialTransaction.java
+++ src/main/java/org/example/SerialTransaction.java
@@ -1,3 +1,9 @@
+/**
+ * The SerialTransaction class provides methods for serializing and deserializing
+ * collections of transactions to and from a file. The file is assumed to contain
+ * comma-separated values for each transaction.
+ */
+
 package org.example;
 
 import java.io.FileNotFoundException;
@@ -17,6 +23,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SerialTransaction {
+    /**
+     * Serializes a collection of transactions to a file.
+     * @param transactions a collection of Transaction objects to serialize
+     * @param filePath the file path where the transactions will be written
+     */
     public void serialize(Collection<Transaction> transactions, Path filePath) {
         try {
             Collection<? extends CharSequence> transactionsToSerialize = transactions
@@ -30,6 +41,12 @@ public class SerialTransaction {
         }
     }
 
+    /**
+     * Deserializes a file of transactions and returns them as a list of Transaction objects.
+     * @param filePath the file path of the file containing the transactions
+     * @return a list of Transaction objects read from the file
+     * @throws IOException if the file does not exist or cannot be read
+     */
     public static List<Transaction> deserialize(Path filePath)
             throws IOException {
         if (Files.notExists(filePath)) {
--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -8,17 +8,38 @@ import java.util.Currency;
 import java.util.Objects;
 
 
+/**
+ * Represents a financial transaction between two parties, consisting of an ID, sender and recipient card numbers,
+ * date and time, amount of money, and currency. This class provides methods for creating new transactions,
+ * copying them, and retrieving transaction details.
+ */
 public class Transaction {
+    /** Inner class providing an empty transaction, used as a default value. */
     private static class EmptyTransaction {
         public static final Transaction emptyTransaction = new Transaction();
     }
+    /** The unique identifier for this transaction. */
     private final long id;
+    /** The card number of the sender for this transaction. */
     private final String senderCardNumber;
+    /** The card number of the recipient for this transaction. */
     private final String recipientCardNumber;
+    /** The date and time this transaction occurred, represented as a ZonedDateTime object. */
     private final ZonedDateTime dateTime; // in format "2011-12-03T10:15:30"
+    /** The amount of money transferred in this transaction, represented as a BigDecimal object. */
     private final BigDecimal moneyAmount;
+    /** The currency of the money transferred in this transaction, represented as a Currency object. */
     private final Currency currency;
 
+    /**
+     * Creates a new Transaction object with the specified values.
+     * @param id                    The unique identifier for this transaction.
+     * @param senderCardNumber      The card number of the sender for this transaction.
+     * @param recipientCardNumber   The card number of the recipient for this transaction.
+     * @param dateTime              The date and time this transaction occurred, represented as a ZonedDateTime object.
+     * @param moneyAmount           The amount of money transferred in this transaction, represented as a BigDecimal object.
+     * @param currency              The currency of the money transferred in this transaction, represented as a Currency object.
+     */
     public Transaction(long id,
                        String senderCardNumber,
                        String recipientCardNumber,
@@ -33,6 +54,10 @@ public class Transaction {
         this.currency = currency;
     }
 
+    /**
+     * Creates a new Transaction object that is a copy of an existing Transaction.
+     * @param transaction The Transaction to be copied.
+     */
     public Transaction(Transaction transaction) {
         this.id = transaction.getId();
         this.senderCardNumber = transaction.getSenderCardNumber();
@@ -42,6 +67,7 @@ public class Transaction {
         this.currency = transaction.getCurrency();
     }
 
+    /** Private constructor used to create an empty Transaction object. */
     private Transaction() {
         this.id = 0;
         this.senderCardNumber = "0000000000000000";
@@ -51,30 +77,58 @@ public class Transaction {
         this.currency = Currency.getInstance("USD");
     }
 
+    /**
+     * Returns an empty Transaction object.
+     * @return An empty Transaction object.
+     */
     public static Transaction emptyTransaction() {
         return EmptyTransaction.emptyTransaction;
     }
 
+    /**
+     * Returns the unique identifier for this transaction.
+     * @return The unique identifier for this transaction.
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Returns the card number of the sender for this transaction.
+     * @return The card number of the sender for this transaction.
+     * */
     public String getSenderCardNumber() {
         return senderCardNumber;
     }
 
+    /**
+     * Returns the card number of the recipient for this transaction.
+     * @return The card number of the recipient for this transaction.
+     * */
     public String getRecipientCardNumber() {
         return recipientCardNumber;
     }
 
+    /**
+     * Returns the date and time of this transaction.
+     * @return the date and time of this transaction.
+     */
     public ZonedDateTime getDateTime() {
         return dateTime;
     }
 
+    /**
+     * Returns the amount of money involved in this transaction.
+     * @return the amount of money involved in this transaction
+     */
     public BigDecimal getMoneyAmount() {
         return moneyAmount;
     }
 
+    /**
+     * Returns the currency used in this transaction.
+     * @return the currency used in this transaction
+     */
     public Currency getCurrency() {
         return currency;
     }
